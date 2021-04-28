936ea96e59ca26d1f79838c3665d8cc095dae4f3
George Ngugi <gngugi17@gmail.com>
2021-04-28 14:27:13 +0300

    update of the code

---
+++ android/app/src/main/kotlin/com/example/valuemissaccoapi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.valuemissaccoapi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/repository/fake_repository.dart
@@ -0,0 +1,41 @@
+import 'package:valuemissaccoapi/model/data_model.dart';
+
+class FakeRepository{
+
+  static List<DataModel> data=[
+    DataModel(
+      serviceName: "Member Registration",
+        time: "9:58 PM",
+        date: "Mon, 31 Aug"
+
+    ),
+    DataModel(
+        serviceName: "Loan Application",
+        time: "9:58 PM",
+        date: "Mon, 31 Aug"
+
+    ),
+    DataModel(
+        serviceName: "View Statement",
+        time: "9:58 PM",
+        date: "Mon, 31 Aug"
+
+    ),
+    DataModel(
+        serviceName: "Account Profile",
+        time: "9:58 PM",
+        date: "Mon, 31 Aug"
+
+    ),
+    DataModel(
+        serviceName: "SIMON",
+        time: "9:58 PM",
+        date: "Mon, 31 Aug"
+    ),
+    DataModel(
+        serviceName: "ZIPPY",
+        time: "9:58 PM",
+        date: "Mon, 31 Aug"
+    ),
+  ];
+}
